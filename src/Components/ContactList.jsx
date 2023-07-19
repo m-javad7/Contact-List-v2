65df377dd4208bee80bac76300847630b2d71b3e
javad7721 <m.javad7721@gmail.com>
2023-07-19 15:39:12 +0330

    Component:fix ContactList

--- src/Components/ContactList.jsx
+++ src/Components/ContactList.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import ContactItem from "./ContactItem";
+
 const CONTACTS_API = "http://localhost:3000/contacts";
 
 const ContactList = () => {
@@ -24,33 +25,31 @@ const ContactList = () => {
     }
   };
 
-  const deleteContact = (id) => {
-    if (window.confirm("Are you sure you want to delete this contact?")) {
-      try {
-        axios.delete(`${CONTACTS_API}/${id}`);
-        setContacts((prevContacts) => prevContacts.filter((contact) => contact.id !== id));
-      } catch (error) {
-        setError("An error occurred while deleting the contact.");
-      }
+  const deleteContact = async (id) => {
+    try {
+      await axios.delete(`${CONTACTS_API}/${id}`);
+      setContacts((prevContacts) => prevContacts.filter((contact) => contact.id !== id));
+    } catch (error) {
+      setError("An error occurred while deleting the contact.");
     }
   };
   
   return (
     <div>
-    {loading ? (
-      <div className="w-full flex flex-col items-center justify-center">
-        <p className="font-bold text-3xl">Loading...</p>
-      </div>
-    ) : error ? (
-      <p className="text-red-600 text-sm">{error}</p>
-    ) : (
-      <>
-        {contacts.map((contact) => (
-          <ContactItem key={contact.id} contact={contact} onDelete={deleteContact} />
-        ))}
-      </>
-    )}
-  </div>
+      {loading ? (
+        <div className="w-full flex flex-col items-center justify-center">
+          <p className="font-bold text-3xl">Loading...</p>
+        </div>
+      ) : error ? (
+        <p className="text-red-600 text-sm">{error}</p>
+      ) : (
+        <>
+          {contacts.map((contact) => (
+            <ContactItem key={contact.id} contact={contact} onDelete={deleteContact} />
+          ))}
+        </>
+      )}
+    </div>
   );
 };
 
